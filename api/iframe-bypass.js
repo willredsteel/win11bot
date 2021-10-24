e17909f2d1f323b48e8ad1339e990fc873795940
andrewstech <info@andrewstech.me>
2021-10-24 17:19:37 +0100

    Update iframe-bypass.js

--- api/iframe-bypass.js
+++ api/iframe-bypass.js
@@ -65,7 +65,7 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 	}
 	fetchProxy (url, options, i) {
 		const proxy = [
-			'https://win11react-proxy.herokuapp.com/'
+			'http://www.whateverorigin.org/get?url='
 		]
 		return fetch(proxy[i] + url, options).then(res => {
 			if (!res.ok)
